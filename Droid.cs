d57a20542cb4a9262e41afdbf8af6840397613b3
KarinWegner <karinwegner00@gmail.com>
2024-12-29 11:51:01 +0100

    Add project files.

---
+++ Droid.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalRegister
+{
+    internal class Droid
+    {
+        private string firstname;
+
+        private int salary;
+
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value < 0)
+                {
+                    salary = 0;
+                }
+                else
+                {
+                    salary = value;
+                }
+            }
+        }
+
+        public int Salary2 { get; set; }
+
+        public static int Count;
+
+        public Droid(string name, int salary)
+        {
+            firstname = name;
+            Salary = salary;
+            Count++;
+        }
+        public Droid() { Count++; }
+
+        public string GetFirstName()
+        {
+            return firstname;
+        }
+        public void SetFirstName(string name)
+        {
+            firstname = name;
+        }
+        public static void GetCount()
+        {
+            Console.WriteLine(Count);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PersonalRegister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Payroll payroll = new Payroll();
+
+            payroll.AddEmployee("Kalle", 20000);
+            payroll.AddEmployee("Anna", 30000);
+
+            IEnumerable<Employee> employees = payroll.GetEmployees();
+
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine($"Name: {employee.Name} Salary: {employee.Salary}");
+            }
+        }
+    }
+}
